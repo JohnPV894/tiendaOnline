c2df862193e0cdbe9bd53c3f2f49a9e649fad80a
John Santiago Arenales Fernandez <johnsaf@aulaestudio.es>
2025-03-11 20:24:45 +0100

    login estructurado con peticiones ajax y recibiendo respuestas validad del php

--- src/backend/validarLogin.php
+++ src/backend/validarLogin.php
@@ -1,19 +1,27 @@
 <?php
 
-#header('Content-Type: application/json; charset=utf-8');
+header('Content-Type: application/json; charset=utf-8');
 require __DIR__ . '/../../vendor/autoload.php';
 $instanciaClienteMongo = new MongoDB\Client("mongodb+srv://santiago894:P5wIGtXue8HvPvli@cluster0.6xkz1.mongodb.net/");
 //Anotacion:Validar cliente mongo
 $coleccion_sesiones = $instanciaClienteMongo->selectDatabase("tiendaOnline")->selectCollection("sesiones");
-
+session_start();
+$_SESSION["POST"]=$_POST;
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-      session_start();
-      $usuario = trim($_POST['usuarioNombre']);
+      #session_start();
+      $usuario = trim($_POST['usuario']);
       $contraseña = trim($_POST['contraseña']);
-
-      $consulta =$coleccion_sesiones ->find( ["usuario" => "santiago894"] )->toArray();
-      echo json_encode($consulta);
+      $consulta =$coleccion_sesiones -> findOne( ["usuario" => $usuario] );#->toArray();
+      #$consulta->acknowledged();
+      #$consulta["esAdmin"],
+      #$esAdmin;
+      #$esLogeado=$consulta["contraseña"]===$contraseña;
+      echo json_encode([
+            #"estadoPeticion"=>true,
+            "esAdmin"=>$consulta,
+            "esLogeado"=>$consulta,
+      ]);
 }else{
-      header("");
-      die("Proceso no Permitido");
+      echo json_encode(["error" => "Proceso no Permitido","status" =>500]);
+      die();
 }
---
+++ src/frontend/scripts/manejoLogin.js
@@ -0,0 +1,42 @@
+"use strict";
+
+$(document).ready(function () {
+      $("#formLogin").submit(function (e) { 
+
+            e.preventDefault();
+            $.ajax({
+                  method: "POST",
+                  url: "http://localhost:3000/src/backend/validarLogin.php",
+                  data:$("#formLogin").serialize(),
+                  //contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+                  success: async function (response) { 
+                        console.log( response);  
+                  },
+                  error: async function (status, error) {
+                        console.error("Error en la petición AJAX:", status, error);
+                  }
+                 });
+           
+
+
+      });
+});
+
+/*
+let respuesta = await response;
+if (!respuesta.message.logeado) {
+      if ($("#error-mensaje").text()=="Error contraseña o Usuario incorrecto") {
+            
+            $("#error-mensaje").empty();
+            $("#error-mensaje").append("Vuelvelo a intentar");
+            
+      }else{
+            $("#error-mensaje").empty();
+            $("#error-mensaje").append("Error contraseña o Usuario incorrecto");
+      }
+      return;
+    
+}
+else {
+      location.assign("http://127.0.0.1:3000/frontend/pages/inicio.html");
+}*/
